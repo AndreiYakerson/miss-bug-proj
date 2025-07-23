8a70e68494e853969e967639bc1ab9cc1af8755d
AndreiYakerson <tubikwheels@gmail.com>
2025-07-23 22:13:32 +0300

    Add demo data to bug.json

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ import { log } from 'console'
 import express from 'express'
 
 const app = express()
+// app.get('/', (req,res) => res.send('Hello there!'))
 
-app.get('/', (req,res) => res.send('Hello there!'))
+app.get('/api/bug', (req, res) => { 
+    
+})
+// app.get('/api/bug/save', (req, res) => { })
+// app.get('/api/bug/:bugId', (req, res) => { })
+// app.get('/api/bug/:bugId/remove', (req, res) => { })
+
 
 const port = 3030
 app.listen(port, () => console.log(`Server is running on http://127.0.0.1:${port}/`))
