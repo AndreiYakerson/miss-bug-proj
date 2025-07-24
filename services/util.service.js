dcb3ec546fe4a35698ccd68232165eaefe79e783
AndreiYakerson <tubikwheels@gmail.com>
2025-07-24 19:50:37 +0300

    Add makeId func to util service

--- services/util.service.js
+++ services/util.service.js
@@ -7,7 +7,8 @@ export const utilService = {
     readJsonFile,
     download,
     writeJsonFile,
-    getRandomIntInclusive
+    getRandomIntInclusive,
+    makeId
 }
 
 
@@ -72,6 +73,16 @@ function getRandomIntInclusive(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-  }
-  
+}
 
+
+function makeId(length = 6) {
+    var txt = ''
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+    for (var i = 0; i < length; i++) {
+        txt += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+
+    return txt
+}
