b91982f6080ad87b98fa84b9c4f346690102a979
AndreiYakerson <tubikwheels@gmail.com>
2025-07-24 23:23:29 +0300

    Prepare bug service for server

---
+++ public/services/bug.service.server.js
@@ -0,0 +1,52 @@
+import { utilService } from './util.service.js'
+import axios from '../lib/axios.js'
+
+const URL = '/api/bug'
+
+export const bugService = {
+    query,
+    getById,
+    save,
+    remove,
+    getDefaultFilter
+}
+
+function query(filterBy) {
+    return axios.get(URL)
+    .then(res => res.data)
+    .then(bugs => {
+
+        if (filterBy.txt) {
+            const regExp = new RegExp(filterBy.txt, 'i')
+            bugs = bugs.filter(bug => regExp.test(bug.title))
+        }
+
+        if (filterBy.minSeverity) {
+            bugs = bugs.filter(bug => bug.severity >= filterBy.minSeverity)
+        }
+
+        return bugs
+    })
+}
+
+function getById(bugId) {
+    return axios.get(`${URL}/${bugId}`)
+    .then(res => res.data)
+}
+
+function remove(bugId) {
+    return axios.get(`${URL}/${bugId}`)
+}
+
+function save(bug) {
+    let queryParams = `/save?title=${bug.title}&severity=${bug.severity}&description=${bug.description}`
+    if (bug._id) queryParams += `&id=${bug._id}`
+
+    return axios.get(`${URL}${queryParams}`)
+    .then(res => res.data)
+}
+
+
+function getDefaultFilter() {
+    return { txt: '', minSeverity: 0 }
+}
