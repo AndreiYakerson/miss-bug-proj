f3cbf8e8a29aaa019f1def8c82df799ab77ba4f6
AndreiYakerson <tubikwheels@gmail.com>
2025-08-11 21:56:08 +0300

    Add creator to BugPreview

--- public/cmps/BugPreview.jsx
+++ public/cmps/BugPreview.jsx
@@ -1,6 +1,7 @@
 export function BugPreview({bug}) {
     return <article className="bug-preview">
         <p className="title">{bug.title}</p>
+        <p>Creator: {bug.creator.fullname}</p>
         <p>Severity: <span>{bug.severity}</span></p>
     </article>
 }
